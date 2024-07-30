e717b7b1cb87aad559c5f3dcb226e1dc038d4a11
himank-17 <155656931+himank-17@users.noreply.github.com>
2024-07-30 13:05:42 +0530

    tutoradd

---
+++ lib/tutor_page.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class TutorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlue[900],
+        title: Text(
+          'BrainSwipe',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Choose Your Role',
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                color: Colors.lightBlue[900],
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Navigate to the Tutor Sign-up or Dashboard Page
+                Navigator.pushNamed(context, '/tutor_signup'); // Adjust route as necessary
+              },
+              style: ElevatedButton.styleFrom(
+                // text color
+                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                textStyle: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+              ),
+              child: Text('I am  Tutor'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Navigate to the Find a Tutor Page
+                Navigator.pushNamed(context, '/find_tutor'); // Adjust route as necessary
+              },
+              style: ElevatedButton.styleFrom(
+                 // background color
+                 // text color
+                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                textStyle: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+              ),
+              child: Text('I Need a Tutor'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
